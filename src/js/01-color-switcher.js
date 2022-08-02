12f13cc86898a7da4871d5cdee1dc79248d05d6c
Lebed Yulia <YuLebed@ukr.net>
2022-08-02 14:18:05 +0300

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
+let timerId = null;
 
+startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {    
+  bodyRef.style.background = getRandomHexColor();     
+  }, 1000);
+  startBtn.setAttribute('disabled', true)
+});
+
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId); 
+  startBtn.removeAttribute('disabled') 
+});
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
